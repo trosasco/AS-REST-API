e327a150082b888a6f4091453ecf5405a354823c
Tessa <trosasco@csumb.edu>
2021-04-15 12:41:43 -0700

    made the smallest change possible

--- app/src/test/java/com/example/week2/ExampleUnitTest.java
+++ app/src/test/java/com/example/week2/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.example.week2;
 
+import android.content.Intent;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +12,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
@@ -30,4 +33,5 @@ public class ExampleUnitTest {
         Users testUser = new Users(0, "test", password);
         assertEquals(testUser.getPassword(), password);
     }
+
 }
